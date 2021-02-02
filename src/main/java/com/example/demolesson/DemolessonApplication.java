d33340f56ab3b44761af237e1d372cfa64d1d081
goga <goga.iremashvili2000@gmail.com>
2021-02-02 21:49:52 +0400

    tutorial

---
+++ src/main/java/com/example/demolesson/DemolessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.demolesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemolessonApplication.class, args);
+    }
+
+}
---
+++ src/main/java/servise/PersonService.java
@@ -0,0 +1,20 @@
+package servise;
+
+import dao.PersonDao;
+import model.Person;
+
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    public PersonService(PersonDao personDao){
+        this.personDao=personDao;
+
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+
+    }
+
+}
