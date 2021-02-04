94c522672171ac380b3c7eb6cd235d0eab95a399
goga <goga.iremashvili2000@gmail.com>
2021-02-04 13:39:00 +0400

    first commit

--- src/main/java/api/PersonController.java
+++ src/main/java/api/PersonController.java
@@ -1,15 +1,21 @@
 package api;
 
 import model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import servise.PersonService;
 
+
+
+@RestController
 public class PersonController {
     private final PersonService personService;
-
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
-
+    @PostMapping
     public void addPerson(Person person){
         personService.addPerson(person);
     }
